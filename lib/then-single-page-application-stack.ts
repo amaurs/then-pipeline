b17c40f2ae79cc0dfd4ac327067bc365938e5332
amaurs <amaury.gtz@gmail.com>
2022-07-30 01:22:18 -0700

    🌎 Adding complete project.

--- lib/then-single-page-application-stack.ts
+++ lib/then-single-page-application-stack.ts
@@ -5,6 +5,33 @@ import { Construct } from 'constructs';
 import { Function, InlineCode, Runtime } from 'aws-cdk-lib/aws-lambda';
 import { aws_cloudfront as cloudfront } from 'aws-cdk-lib';
 import { aws_codebuild as codebuild } from 'aws-cdk-lib';
+import * as yaml from 'yaml';
+
+const fromYaml = yaml.parse(`
+version: 0.2
+phases:
+  install:
+    runtime-versions:
+      nodejs: 16
+  pre_build:
+    commands:
+      - npm
+  build:
+    commands:
+      - echo $REACT_APP_API_HOST
+      - echo "if-then"
+      - mkdir -p src/fonts
+      - aws s3 sync s3://$FONT_S3_BUCKET/ src/fonts
+      - npm install terser@3.14.1 --save-dev
+      - npm install
+      - npm run build
+      # - command
+  post_build:
+    commands:
+      - aws s3 sync --cache-control 'max-age=604800' --exclude index.html build/ s3://$HOME_SWEET_HOME/
+      - aws s3 sync --cache-control 'no-cache' build/ s3://$HOME_SWEET_HOME/
+
+`);
 
 
 export class ThenSinglePageApplicationStack extends cdk.Stack {
@@ -47,5 +74,26 @@ export class ThenSinglePageApplicationStack extends cdk.Stack {
             reportBuildStatus: true,
         });
 
+        console.log('HOME_SWEET_HOME 👉🏽', bucket.bucketName);
+        console.log('REACT_APP_API_HOST 👉🏽', process.env.REACT_APP_API_HOST);
+        console.log('REACT_APP_GA_ID 👉🏽', process.env.REACT_APP_GA_ID);
+        console.log('FONT_S3_BUCKET 👉🏽', process.env.FONT_S3_BUCKET);
+
+        const project = new codebuild.Project(this, `then.gallery-build`, {
+              buildSpec: codebuild.BuildSpec.fromObjectToYaml(fromYaml),
+              projectName: `$then.gallery-build`,
+              environment: {
+                  buildImage: codebuild.LinuxBuildImage.STANDARD_3_0,
+                  computeType: codebuild.ComputeType.SMALL,
+              },
+              environmentVariables: {
+                'HOME_SWEET_HOME': {value: bucket.bucketName},
+                'REACT_APP_API_HOST': {value: process.env.REACT_APP_API_HOST!},
+                'REACT_APP_GA_ID': {value: process.env.REACT_APP_GA_ID!},
+                'FONT_S3_BUCKET': {value: process.env.FONT_S3_BUCKET!},
+              },
+              source: repo,
+              timeout: cdk.Duration.minutes(20),
+          });
     }
 }
