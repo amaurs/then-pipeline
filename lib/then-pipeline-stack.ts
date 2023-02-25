ff61228998e45c879c440fc9ad7f29dce5d44863
amaurs <amaury.gtz@gmail.com>
2023-02-25 13:38:22 -0800

    📝 Adding blog subdomain.

--- lib/then-pipeline-stack.ts
+++ lib/then-pipeline-stack.ts
@@ -35,7 +35,6 @@ export class ThenPipelineStack extends Stack {
             'REGION': process.env.REGION!,
             'GITHUB_PERSONAL_ACCESS_TOKEN_SECRET_NAME': process.env.GITHUB_PERSONAL_ACCESS_TOKEN_SECRET_NAME!,
             'REACT_APP_API_HOST': process.env.REACT_APP_API_HOST!,
-            'REACT_APP_API_BANDIT_HOST': process.env.REACT_APP_API_BANDIT_HOST!,
             'REACT_APP_GA_ID': process.env.REACT_APP_GA_ID!,
             'FONT_S3_BUCKET': process.env.FONT_S3_BUCKET!,
         },
