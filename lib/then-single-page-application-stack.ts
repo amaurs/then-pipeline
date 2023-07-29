b14c7beaecb750e867677686ebb5012e747cf6e7
amaurs <amaury.gtz@gmail.com>
2023-07-29 00:19:42 -0700

    💐 Adding into domain for show.

--- lib/then-single-page-application-stack.ts
+++ lib/then-single-page-application-stack.ts
@@ -19,6 +19,7 @@ export class ThenSinglePageApplicationStack extends cdk.Stack {
         const primaryDomain = 'then.gallery';
         const blogDomain = `blog.${primaryDomain}`;
         const aboutDomain = `about.${primaryDomain}`;
+        const intoDomain = `into.${primaryDomain}`;
 
         console.log('Creating the bucket');
         const bucket = new s3.Bucket(this, "ThenSinglePageApplicationBucket", {
@@ -55,6 +56,7 @@ export class ThenSinglePageApplicationStack extends cdk.Stack {
             subjectAlternativeNames: [
                 blogDomain,
                 aboutDomain,
+                intoDomain,
                 ],
             validation: certificate_manager.CertificateValidation.fromDns(hostedZone),
         });
@@ -71,7 +73,7 @@ export class ThenSinglePageApplicationStack extends cdk.Stack {
                 },
             ],
             viewerCertificate: {
-                aliases: [primaryDomain, blogDomain, aboutDomain],
+                aliases: [primaryDomain, blogDomain, aboutDomain, intoDomain],
                 props: {
                     acmCertificateArn: certificate.certificateArn,
                     sslSupportMethod: 'sni-only',
@@ -115,6 +117,12 @@ export class ThenSinglePageApplicationStack extends cdk.Stack {
         new route53.ARecord(this, 'ThenAboutDomainAlias', {
             zone: hostedZone,
             recordName: `about.${primaryDomain}`,
+            target: route53.RecordTarget.fromAlias(new route53_targets.CloudFrontTarget(cloudfrontDist)),
+        });
+
+        new route53.ARecord(this, 'ThenIntoDomainAlias', {
+            zone: hostedZone,
+            recordName: `into.${primaryDomain}`,
             target: route53.RecordTarget.fromAlias(new route53_targets.CloudFrontTarget(cloudfrontDist)),
         });
 
